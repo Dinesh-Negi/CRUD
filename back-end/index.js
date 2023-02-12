ee0bc86f03c891422afef5b8de90e57b51528b4b
@DN3107 <negidinesh2001@gmail.com>
2023-02-12 19:13:53 +0530

    first commit

--- back-end/index.js
+++ back-end/index.js
@@ -12,7 +12,7 @@ mongoose
   .connect("mongodb://0.0.0.0:27017/myDB")
   .catch((err) => console.log(err));
 
-// Data Base SCHEMA AND MODEL
+// This is a Data Base SCHEMA AND MODEL
 const postSchema = mongoose.Schema({
   title: String,
   description: String,
